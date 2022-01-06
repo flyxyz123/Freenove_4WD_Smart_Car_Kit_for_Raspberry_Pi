07f473725710d46d57f2e988415230681adaaab9
xyz <gky44px1999@gmail.com>
2022-01-05 23:21:47 -0800

    refactor

--- Code/Server/voice.py
+++ Code/Server/voice.py
@@ -58,8 +58,8 @@ while True:
         #pi_ear.pause_thpi_eareshold=1
         pi_ear.dynamic_energy_threshold = True
         #pi_ear.energy_threshold = 100
-        pi_ear.pause_threshold = 0.6
-        pi_ear.adjust_for_ambient_noise(source, duration=0.5)
+        pi_ear.pause_threshold = 0.4
+        pi_ear.adjust_for_ambient_noise(source, duration=0.3)
         print("\033[0;35mpi: \033[0m I'm listening")
         audio = pi_ear.listen(source)
     try:
