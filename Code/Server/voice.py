ba213b16dcd9cb9debaef4bea196540857008a14
xyz <gky44px1999@gmail.com>
2022-01-05 23:31:56 -0800

    refactor

--- Code/Server/voice.py
+++ Code/Server/voice.py
@@ -59,12 +59,13 @@ while True:
         #pi_ear.dynamic_energy_threshold = True
         #pi_ear.energy_threshold = 100
         #pi_ear.pause_threshold = 0.6
-        #pi_ear.adjust_for_ambient_noise(source, duration=0.5)
+        pi_ear.adjust_for_ambient_noise(source, duration=0.5)
         print("\033[0;35mpi: \033[0m I'm listening")
         audio = pi_ear.listen(source)
+        print("finish listen")
     try:
-        you = pi_ear.recognize_google(audio)
-        #you = pi_ear.recognize_sphinx(audio)
+        #you = pi_ear.recognize_google(audio)
+        you = pi_ear.recognize_sphinx(audio)
     except:
         you = ""
     print(you)
