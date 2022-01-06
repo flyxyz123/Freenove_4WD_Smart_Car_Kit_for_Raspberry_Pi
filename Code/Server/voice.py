16988da0114a92096daa7fce8c5f5ce0ac1700cb
xyz <gky44px1999@gmail.com>
2022-01-05 23:46:21 -0800

    refactor

--- Code/Server/voice.py
+++ Code/Server/voice.py
@@ -56,7 +56,7 @@ while True:
     with mic as source:
         #pi_ear.pause_thpi_eareshold=1
         #pi_ear.dynamic_energy_threshold = True
-        #pi_ear.energy_threshold = 100
+        pi_ear.energy_threshold = 100
         #pi_ear.pause_threshold = 0.6
         pi_ear.adjust_for_ambient_noise(source, duration=0.5)
         print("\033[0;35mpi: \033[0m I'm listening")
